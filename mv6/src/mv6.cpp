de5fb59ca5f35fde7a895c8924f55ee4ece37d2b
Sriee <psg.sriee@gmail.com>
2015-11-23 19:25:25 -0600

    Initial commit for the project

---
+++ mv6/src/mv6.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : mv6.cpp
+// Author      : Sriee
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Git is cool!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
