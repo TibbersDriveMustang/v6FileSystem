65a53b23bd6bf3653990129bb7bb10fa413693c8
Sriee <psg.sriee@gmail.com>
2015-11-24 14:57:23 -0600

    Add till initfs entry validation
    Integrated the constants and logger files. Checked the parameters for
    initfs command

--- mv6/src/mv6.cpp
+++ mv6/src/mv6.cpp
@@ -7,9 +7,103 @@
 //============================================================================
 
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include "Constants.h"
+#include "Log.h"
+
 using namespace std;
 
-int main() {
-	cout << "!!!Git is cool!!!" << endl; // prints !!!Hello World!!!
+Log log;
+
+class Entry{
+	string command;
+	int commandType=0;
+public:
+	Entry(string command){
+			this->command = command;
+	}
+	bool isValidCommand(string command);
+	void printCommand(void);
+	void setCommandType(int value);
+	int getCommandType();
+
+};
+
+void Entry::printCommand(){
+	cout <<"The Entered command is = " <<this->command <<endl;
+}
+
+void Entry::setCommandType(int value){
+	this->commandType = value;
+}
+
+int Entry::getCommandType(){
+	return this->commandType;
+}
+bool Entry::isValidCommand(string command){
+	bool valid = false;
+	if (INITFS.compare(command)==0){
+		valid = true;
+		this->setCommandType(static_cast<int>(initfs));
+	}else if (CP_IN.compare(command)==0){
+		valid = true;
+		this->setCommandType(static_cast<int>(cpin));
+	}else if (CP_OUT.compare(command)==0){
+		valid = true;
+		this->setCommandType(static_cast<int>(cpout));
+	}else if (MK_DIR.compare(command)==0){
+		valid = true;
+		this->setCommandType(static_cast<int>(mkdir));
+	}else if (Q.compare(command)==0){
+		valid = true;
+		this->setCommandType(static_cast<int>(q));
+	}else{
+		valid = false;
+	}
+	return valid;
+}
+
+class InitializeFS{
+public:
+	void checkParameters(int argc, char *argv[]);
+};
+
+void InitializeFS :: checkParameters(int argc, char *argv[]){
+	string fsPath;
+	int numOfBlocks,numOfinodes;
+	if (argc < 5){
+		cout <<"!!Insufficient number of arguments!!" <<endl;
+	}else{
+		fsPath = argv[2];
+		numOfBlocks = atoi(argv[3]);
+		numOfinodes = atoi(argv[4]);
+		log.logger(82,"Argument Count",argc);
+		log.logger(83,"Path",fsPath);
+		log.logger(84,"First argument",numOfBlocks);
+		log.logger(85,"Second argument",numOfinodes);
+	}
+}
+
+int main(int argc,char *args[]) {
+
+	string firstArgument=args[1];
+	Entry entry(firstArgument);
+
+	if(entry.isValidCommand(firstArgument)){
+		cout<<"!!Entered Valid Command!!" <<endl;
+	}else{
+		cout<<"!!Entered Invalid Command!!\nTry Again...." <<endl;
+		exit(0);
+	}
+	switch(entry.getCommandType()){
+		case initfs: InitializeFS fs;
+					 fs.checkParameters(argc,args);
+					 break;
+		case cpin:  entry.printCommand(); break;
+		case cpout: entry.printCommand(); break;
+		case mkdir: entry.printCommand(); break;
+		default: cout<<"!!Entered Invalid Command!!\nTry Again...." <<endl; break;
+	}
 	return 0;
 }
