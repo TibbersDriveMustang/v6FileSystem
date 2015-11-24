4122ee4de96fa89cfd4f791b3f4578cc0f865cd4
Sriee <psg.sriee@gmail.com>
2015-11-24 14:54:48 -0600

    Add Log class files
    Log files have logger helper methods which helps to display the log
    messages in the format I want. It is also easy to find and delete the
    log messages when I am done debuging the file

---
+++ mv6/src/Log.cpp
@@ -0,0 +1,26 @@
+/*
+ * Log.cpp
+ *
+ *  Created on: 24-Nov-2015
+ *      Author: sriee
+ */
+
+#include "Log.h"
+#include <iostream>
+
+Log::Log() {
+	// TODO Auto-generated constructor stub
+	std::cout << "Logger is present in the code!!!" <<std::endl;
+}
+
+void Log::logger(int lineNumber, std::string comment){
+	std::cout <<"***" <<lineNumber <<" " <<comment << std::endl;
+}
+
+void Log::logger(int lineNumber, std::string comment,int arg){
+	std::cout <<"***" <<lineNumber <<" " <<comment <<" = " <<arg << std::endl;
+}
+
+void Log::logger(int lineNumber, std::string comment,std::string arg){
+	std::cout <<"***" <<lineNumber <<" " <<comment <<" = " <<arg << std::endl;
+}
---
+++ mv6/src/Log.h
@@ -0,0 +1,19 @@
+/*
+ * Log.h
+ *
+ *  Created on: 24-Nov-2015
+ *      Author: sriee
+ */
+
+#include <string>
+#ifndef LOG_H_
+#define LOG_H_
+
+class Log {
+public:
+	Log();
+	void logger(int lineNumber, std::string comment);
+	void logger(int lineNumber, std::string comment,int arg);
+	void logger(int lineNumber, std::string comment,std::string arg);
+};
+#endif /* LOG_H_ */
